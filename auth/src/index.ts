6a76ca555cfffe24038fae0fd76135401dee5e47
Leftyxiv <manny@aledoux.net>
2021-04-24 08:20:49 -0400

    modified skaffold config, can access container via browser

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hallo, wunderbar')
-})
 
-app.listen(9000, ()=> {
-  console.log('listening on port 9000 ***LOVE***')
-})
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hallo, wunderbar");
+});
+
+app.listen(9000, () => {
+  console.log("listening on port 9000 **LOVE**");
+});
