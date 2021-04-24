1bc5cdd25a938c8c4dbcc5b8e6dfe6625d396520
Leftyxiv <manny@aledoux.net>
2021-04-24 16:17:20 -0400

    added requestvalidationerror class

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,23 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/api/users/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Not a valid email'),
-  body('password')
-    .trim()
-    .isLength({ min: 4, max: 20})
-    .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    throw new Error('invalid email or password')
-  }
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Not a valid email"),
+    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new Error("invalid email or password");
+    }
+    const { email, password } = req.body;
 
-res.send('alelelele')
-})
+    res.send("alelelele");
+  },
+);
 
 export { router as signupRouter };
