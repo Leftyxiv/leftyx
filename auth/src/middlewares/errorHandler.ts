9c747a74938d2303f0bd055e94be1852ebcfa879
Leftyxiv <manny@aledoux.net>
2021-04-24 17:47:01 -0400

    refactored some error handl,ing logic

--- auth/src/middlewares/errorHandler.ts
+++ auth/src/middlewares/errorHandler.ts
@@ -1,18 +1,15 @@
-import { Request, Response, NextFunction } from 'express';
-import { DatabaseConnectionError } from '../errors/databaseConnectionError';
-import { RequestValidationError } from '../errors/requestValidationError';
+import { Request, Response, NextFunction } from "express";
+import { DatabaseConnectionError } from "../errors/databaseConnectionError";
+import { RequestValidationError } from "../errors/requestValidationError";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-if (err instanceof RequestValidationError){
-  const formattedErrors = err.errors.map(error => {
-    return { message: error.msg, field: error.param }
-  })
-  return res.status(400).send({ errors: formattedErrors })
-}
-if (err instanceof DatabaseConnectionError){
-  console.log('handling this error as a db connection error')
-}
-res.status(400).send({
-  message: err.message
-})
-}
+  if (err instanceof RequestValidationError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors()});
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
+  }
+  res.status(400).send({
+    errors: ({ message: 'Something went very wrong!'})
+  });
+};
