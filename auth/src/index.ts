5af0b4eb2ee9bb784f0cd985e6235b3fee24d175
Leftyxiv <manny@aledoux.net>
2021-04-24 08:10:08 -0400

    added ingress service config file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hallo, wunderbar')
+})
+
 app.listen(9000, ()=> {
-  console.log('listening on port 9000')
+  console.log('listening on port 9000 ***LOVE***')
 })
