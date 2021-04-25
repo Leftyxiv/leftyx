0bffa59e60a6181fdd225a2c003a1ad32dc5de59
Leftyxiv <manny@aledoux.net>
2021-04-25 19:46:48 -0400

    added get current user route

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-res.send('Hallo, wunderbar')
+if(!req.session?.jwt){
+  return res.send({ currentUser: null })
+}
+try{
+  const payload = jwt.verify(req.session.jwt, process.env.jwt_key!)
+  res.send({ currentUser: payload })
+} catch (err) {
+  res.send({ currentUser: null })
+}
 })
 
 export { router as currentUserRouter };
