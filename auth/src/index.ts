ea81d395c6f63dca55bb3a0db6f5eec5d56cea62
Leftyxiv <manny@aledoux.net>
2021-04-24 20:14:03 -0400

    added express async errors module

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/currentUser'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/errorHandler';
+import { NotFoundError } from './errors/notFoundError';
 
 const app = express();
 app.use(express.json());
@@ -13,7 +15,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async () => {
+   throw new NotFoundError();
+})
 app.use(errorHandler);
+
 
 
 app.listen(9000, () => {
