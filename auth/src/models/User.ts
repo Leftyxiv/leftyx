9e355a0203dce409264b8b929db60677fed21195
Leftyxiv <manny@aledoux.net>
2021-04-25 09:04:27 -0400

    added user document interface

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -6,8 +6,13 @@ interface UserAttrs {
 }
 
 // user model interface
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -25,7 +30,7 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 
 export { User };
