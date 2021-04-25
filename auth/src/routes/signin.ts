6dc6debd5ef99f17c2b930e2b656c14752495c53
Leftyxiv <manny@aledoux.net>
2021-04-25 19:22:20 -0400

    added functionality for the sign in route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response} from "express";
 import { body } from 'express-validator';
-
+import jwt from 'jsonwebtoken';
 import { User } from '../models/User';
-import { validateRequest } from './../middlewares/validateRequest';
+import { validateRequest } from '../middlewares/validateRequest';
+import { BadRequestError } from '../errors/badRequestErrors';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -10,10 +12,26 @@ router.post("/api/users/signin", [
   body('email').isEmail().withMessage('Email ,ust be valid'),
   body('password').trim().notEmpty().withMessage('You must supply a password')
 ], validateRequest, async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email })
+  if (!existingUser){
+    throw new BadRequestError('Login attempt failed!  Please try again')
+  }
+  const passwordsMatch = await Password.compare(existingUser.password, password);
+  if (!passwordsMatch){
+    throw new BadRequestError('Login attempt failed!  Please try again')
+  }
 
-  const user = await User.findOne({ email: req.body.email })
+  const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+  }, process.env.jwt_key! )
 
-  res.send('good')
+  req.session= {
+    jwt: userJwt
+  };
+
+  res.status(200).send(existingUser);
 });
 
 export { router as signinRouter };
