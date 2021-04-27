4117b7500bdb05e714cf6106ac80b948647ec259
Leftyxiv <manny@aledoux.net>
2021-04-27 14:44:51 -0400

    sign in page is in and working

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import buildClient from '../api/buildClient';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
   return (
     <div>
-      <h1>landing page</h1>
+     <h1 style={{ color: 'rebeccapurple' }}>
+       { currentUser ? 'You are signed in' : 'You are not signed in'}
+       </h1> 
     </div>
   );
 };
 
 LandingPage.getInitialProps = async (context) => {
- const { data } = await buildClient(context).get('/api/users/currentuser')
+  const client =  buildClient(context)
+ const { data } = await client.get('/api/users/currentuser')
  return data
 };
 
