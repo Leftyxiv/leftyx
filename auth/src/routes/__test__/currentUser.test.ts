b07f69d5a03e4e369be31115f5f3c31be92c743a
Leftyxiv <manny@aledoux.net>
2021-04-27 09:18:34 -0400

    added testing for current user route handler

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -3,11 +3,12 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user", async () => {
-  const authRes = await request(app)
-    .post("/api/users/signup")
-    .send({ email: "test@test.com", password: "pass" })
-    .expect(201);
-  const cookie = authRes.get("Set-Cookie");
+  const cookie = await global.signin();
   const response = await request(app).get("/api/users/currentuser").set("Cookie", cookie).expect(200);
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responds with current user of null", async () => {
+  const res = await request(app).get("/api/users/currentuser").expect(200);
+  expect(res.body.currentUser).toEqual(null);
 });
