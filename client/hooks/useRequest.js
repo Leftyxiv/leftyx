9b504f399e34feaba9f317805e5eecc840534938
Leftyxiv <manny@aledoux.net>
2021-04-27 11:48:44 -0400

    sign up redirects to landing page after signup

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -1,25 +1,29 @@
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useState } from "react";
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
+      setErrors(null);
       const res = await axios[method](url, body);
-      return res.data
+      if (onSuccess){
+        onSuccess(res.data)
+      }
+      return res.data;
     } catch (err) {
       setErrors(
         <div className="alert alert-danger">
-        <h4>Oops!</h4>
-        <ul className="my-0">
-          {err.response.data.errors.map((err) => (
-            <li key={err.message}>{err.message}</li>
-          ))}
-        </ul>
-      </div>
+          <h4>Oops!</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>,
       );
     }
   };
-  return { doRequest, errors }
-  }
+  return { doRequest, errors };
+};
