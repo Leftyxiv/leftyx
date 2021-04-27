57e44d0454b0b564e21d48192b268c78741a6cb1
Leftyxiv <manny@aledoux.net>
2021-04-27 13:06:37 -0400

    initial props now sends request properly from the client side in index for client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-const index = () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div>
-      <h1>landing page 656</h1>
+      <h1>landing page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default index
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // code here executed server side
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "leftyx.dev",
+        },
+      },
+    );
+    return data;
+  } else {
+    // code executed inside the browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  // return res.data;
+  return {};
+};
+
+export default LandingPage;
