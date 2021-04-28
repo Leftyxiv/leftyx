cbbb2de6038c572ca4a481f7d7d46613d107def9
Leftyxiv <manny@aledoux.net>
2021-04-28 07:42:25 -0400

    first ticket test passes

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,11 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("has a router handler listening to /api/tickets for post requests", async () => {});
+it("has a router handler listening to /api/tickets for post requests", async () => {
+  const res = await request(app).post('/api/tickets')
+  .send({})
+  expect(res.status).not.toEqual(404);
+});
 
 it("can only be accessed by authenticated user", async () => {});
 
