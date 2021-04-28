8246deb0ab7c105efab4737c4b88f675009da634
Leftyxiv <manny@aledoux.net>
2021-04-28 07:36:13 -0400

    added tests for tickets

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("has a router handler listening to /api/tickets for post requests", async () => {});
+
+it("can only be accessed by authenticated user", async () => {});
+
+it("returns an error if an invalid title is provided", async () => {});
+
+it("returns an error if an invalid price is provided", async () => {});
+
+it("creates a ticket with valid inputs", async () => {});
