97be0336e958d03ea9ab40b2d3f095f55bef431e
Leftyxiv <manny@aledoux.net>
2021-04-28 07:27:15 -0400

    moved mongo uri to env variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,24 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import { app } from './app';
+import { app } from "./app";
 
 const start = async () => {
-  if (!process.env.jwt_key){
-    throw new Error('jwt key not defined')
+  if (!process.env.jwt_key) {
+    throw new Error("jwt key not defined");
   }
-  try{
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+  if (!process.env.MONGO_URI) {
+    throw new Error("mongo_uri must be defined");
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    console.log('Mongodb connected...')
+      useCreateIndex: true,
+    });
+    console.log("Mongodb connected...");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
   app.listen(9000, () => {
     console.log("listening on port 9000 **LOVE**");
   });
-}
+};
 
 start();
