b293d50fb8617a1ff6142520bd02aab5797c21ec
Leftyxiv <manny@aledoux.net>
2021-04-28 08:17:34 -0400

    added logic for field validation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -11,13 +11,45 @@ it("can only be accessed by authenticated user", async () => {
 });
 
 it("returns a status other than 401 if authenticated", async () => {
-  const res = await request(app).post("/api/tickets").send({})
+  const res = await request(app).post("/api/tickets").set('Cookie', global.signin()).send({})
   
   expect(res.status).not.toEqual(401);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: '',
+    price: 10
+  })
+  .expect(400)
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    price: 10
+  })
+  .expect(400)
+});
 
-it("returns an error if an invalid price is provided", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: 'lefty\'s tix',
+    price: -4
+  })
+  .expect(400)
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    'title': 'lefty\'s tix',
+  })
+  .expect(400)
+});
 
 it("creates a ticket with valid inputs", async () => {});
