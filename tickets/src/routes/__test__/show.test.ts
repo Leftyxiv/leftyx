29824a213fd18fa450308598edccd2ed6d8d0555
Leftyxiv <manny@aledoux.net>
2021-04-28 11:12:20 -0400

    finished show ticket route

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,10 @@
+import  mongoose  from 'mongoose';
 import request from "supertest";
 import { app } from "../../app";
 
 it("returns a 404 if ticket is no found", async () => {
-  await request(app).get("/api/tickets/e543w424324").send().expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns a the ticket if the ticket is found", async () => {
--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
-import { requireAuth, validateRequest } from "@leftyx/common"
+import {NotFoundError} from '@leftyx/common';
 import { Ticket } from '../models/Ticket';
 
 const router = express.Router()
+
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket){
+    throw new NotFoundError();
+  }
+
+res.send(ticket)
+})
+
+export { router as showTicketRouter };
