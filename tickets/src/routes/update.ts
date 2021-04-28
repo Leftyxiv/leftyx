26f23767989dac506b5e1cee9d5489c0f9983149
Leftyxiv <manny@aledoux.net>
2021-04-28 11:46:39 -0400

    update ticket route passes first two tests

---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest, NotFoundError, NotAuthorizedError } from "@leftyx/common"
+import { Ticket } from '../models/Ticket';
+
+const router = express.Router();
+
+router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id)
+  if (!ticket){
+    throw new NotFoundError();
+  }
+  res.send(ticket)
+})
+
+export { router as updateTicketRouter };
