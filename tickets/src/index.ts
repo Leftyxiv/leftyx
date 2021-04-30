78f3294396096433ed486e198b4fa3c8abf9722f
Leftyxiv <manny@aledoux.net>
2021-04-30 13:03:59 -0400

    Nats can shut down gracefully

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import { nanoid } from 'nanoid'
 
 import { app } from "./app";
+import { natsWrapper } from './natsWrapper';
+
 
 const start = async () => {
   if (!process.env.jwt_key) {
@@ -10,6 +13,22 @@ const start = async () => {
     throw new Error("mongo_uri must be defined");
   }
   try {
+    await natsWrapper.connect('leftyx', nanoid(), 'http://nats-srv:4222');
+
+    natsWrapper.client.on("close", () => {
+      console.log("nats connection closed");
+      // @ts-ignore
+      process.exit();
+    });
+
+  // @ts-ignore
+  process.on("SIGNINT", () => natsWrapper.client.close());
+  // @ts-ignore
+  process.on("SIGTERM", () => natsWrapper.client.close());
+  
+
+
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -4,6 +4,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from "@leftyx/common"
 import { Ticket } from '../models/Ticket';
 import { TicketCreatedPublisher } from '../events/publishers/TicketCreatedPublisher';
+import { natsWrapper } from '../natsWrapper';
 
 
 const router = express.Router();
@@ -15,7 +16,7 @@ router.post('/api/tickets', requireAuth, [
   const { title, price } = req.body;
   const ticket = Ticket.build({title, price, userId: req.currentUser!.id})
   await ticket.save()
-  new TicketCreatedPublisher(client).publish({
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
     id: ticket.id,
     title: ticket.title,
     price: ticket.price,
