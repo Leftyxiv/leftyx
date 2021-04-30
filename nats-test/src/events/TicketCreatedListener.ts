2f8c0f290edc2e5f8a1d01b38ad5972fecf48832
Leftyxiv <manny@aledoux.net>
2021-04-30 10:33:00 -0400

    added some interfaces and type checks

--- nats-test/src/events/TicketCreatedListener.ts
+++ nats-test/src/events/TicketCreatedListener.ts
@@ -1,9 +1,12 @@
+import { Subjects } from './subjects';
 import { Message } from 'node-nats-streaming';
 import { Listener } from './baseListener';
+import { TicketCreatedEvent } from './TicketCreatedEvent';
 
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'paymentsService';
   onMessage(data: any, msg: Message){
     console.log('event data', data)
