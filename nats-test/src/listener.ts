c1b0f0cd4ac3069a5a941e24b60d4beaefa2606f
Leftyxiv <manny@aledoux.net>
2021-04-30 08:13:59 -0400

    added ticketcreatedlistener class and is in and functioning

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,21 +14,10 @@ stan.on("connect", () => {
     // @ts-ignore
     process.exit();
   });
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("orderService");
 
-  const subscription = stan.subscribe("ticket:created", "queueGroupName", options);
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
+  new TicketCreatedListener(stan).listen();
 
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+
 });
 // @ts-ignore
 process.on("SIGNINT", () => stan.close());
@@ -38,6 +27,7 @@ process.on("SIGTERM", () => stan.close());
 abstract class Listener {
   abstract subject: string;
   abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
@@ -52,5 +42,29 @@ abstract class Listener {
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
       .setDurableName(this.queueGroupName);
+  }
+  listen() {
+    const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.subscriptionOptions())
+    
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg)
+    })
+    
+  }
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'paymentsService';
+  onMessage(data: any, msg: Message){
+    console.log('event data', data)
+    msg.ack()
   }
 }
