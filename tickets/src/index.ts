40065c3eb01e6f68c26c34091e8a591c97e5c22d
Leftyxiv <manny@aledoux.net>
2021-05-01 06:56:19 -0400

    ticket service looks to be just about done

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,8 +12,17 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("mongo_uri must be defined");
   }
+  if (!process.env.NATS_URL) {
+    throw new Error("nats_url must be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("nats client id must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("nats cluster id must be defined");
+  }
   try {
-    await natsWrapper.connect('leftyx', nanoid(), 'http://nats-srv:4222');
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
 
     natsWrapper.client.on("close", () => {
       console.log("nats connection closed");
