bf255ce6d82e04ae6743642432f5340f3f0d3349
Leftyxiv <manny@aledoux.net>
2021-05-01 15:21:12 -0400

    added order status enum to common

---
+++ orders/src/models/Order.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { OrderStatus } from '@leftyx/common';
+
+
+interface OrdersAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// user model interface
+
+interface OrdersDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrdersModel extends mongoose.Model<OrdersDoc> {
+  build(attrs: OrdersAttrs): OrdersDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  },
+);
+
+
+orderSchema.statics.build = (attrs: OrdersAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrdersDoc, OrdersModel>("Orders", orderSchema);
+
+export { Order };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,15 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from "@leftyx/common"
+import { body } from 'express-validator';
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+    .not().isEmpty().custom((input: string) => mongoose.Types.ObjectId.isValid(input)).withMessage('a ticket id must be provided')
+  
+], validateRequest, async (req: Request, res: Response) => {
   res.send({})
 })
 
