2b2cebb0341e4a287a1fe2aafcac6625e26f3f29
Leftyxiv <manny@aledoux.net>
2021-05-02 07:18:33 -0400

    added show route handler

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
   const orders = Order.find({ userId: req.currentUser!.id }).populate('ticket');
-  res.send({})
+  res.send(orders)
 })
 
 export { router as indexOrdersRouter }
