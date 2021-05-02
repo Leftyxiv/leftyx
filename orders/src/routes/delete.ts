771e0fc0f60ef9f13656f066c3608783535247e8
Leftyxiv <manny@aledoux.net>
2021-05-02 07:30:55 -0400

    added delete order route handler

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from "express";
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@leftyx/common';
+import { Order, OrderStatus } from '../models/Order';
+
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId);
+  if (!order){
+ throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+  res.send(order);
 });
 
 export { router as deleteOrdersRouter };
