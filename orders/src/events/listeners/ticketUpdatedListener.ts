5458f2ffc5274e513428d8513c46e689b00a5ef6
Leftyxiv <manny@aledoux.net>
2021-05-03 11:56:55 -0400

    added ticketupdated listener

---
+++ orders/src/events/listeners/ticketUpdatedListener.ts
@@ -0,0 +1,22 @@
+import { Message } from "node-nats-streaming";
+
+import { Subjects, Listener, TicketUpdatedEvent } from "@leftyx/common";
+import { Ticket } from "../../models/Ticket";
+import { queueGroupName } from "./queueGroupName";
+
+export class TickertUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const { title, price } = data;
+    const ticket = await Ticket.findById(data.id);
+    if(!ticket){
+      throw new Error('Ticket not found');
+    }
+    ticket.set({ title, price });
+    await ticket.save();
+
+
+    msg.ack();
+  }
+}
