43d87d5fd99a149c1eadb3cc5a92635a405377f2
Leftyxiv <manny@aledoux.net>
2021-05-05 16:36:51 -0400

    updated common module and added ticket updated publisher to the order created listener

--- tickets/src/events/listeners/OrderCreatedListener.ts
+++ tickets/src/events/listeners/OrderCreatedListener.ts
@@ -1,18 +1,28 @@
-import { Message } from 'node-nats-streaming';
-import { Listener, OrderCreatedEvent, Subjects } from '@leftyx/common';
-import { queueGroupName } from './queueGroupName';
-import { Ticket } from '../../models/Ticket';
+import { Message } from "node-nats-streaming";
+import { Listener, OrderCreatedEvent, Subjects } from "@leftyx/common";
+import { queueGroupName } from "./queueGroupName";
+import { Ticket } from "../../models/Ticket";
+import { TicketUpdatedPublisher } from "../publishers/TicketUpdatedPublisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     const ticket = await Ticket.findById(data.ticket.id);
-    if (!ticket){
-      throw new Error('Ticket not found')
+    if (!ticket) {
+      throw new Error("Ticket not found");
     }
     ticket.set({ orderId: data.id });
     await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
+
     msg.ack();
   }
 }
