37f6ec2331aefcfb491ef2d01455944bf6b1b04d
Leftyxiv <manny@aledoux.net>
2021-05-06 08:51:41 -0400

    added listeners to the nats client

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 import { app } from "./app";
 import { natsWrapper } from './natsWrapper';
+import { OrderCreatedListener } from './events/listeners/OrderCreatedListener';
+import { OrderCancelledListener } from './events/listeners/OrderCancelledListener';
+
 
 
 const start = async () => {
@@ -34,8 +37,8 @@ const start = async () => {
   // @ts-ignore
   process.on("SIGTERM", () => natsWrapper.client.close());
   
-
-
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
