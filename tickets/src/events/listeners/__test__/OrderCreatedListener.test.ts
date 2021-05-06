a340798530be59f44a1ab9c35dfbb3f0ff982cca
Leftyxiv <manny@aledoux.net>
2021-05-06 08:28:33 -0400

    added tests for order created listener

--- tickets/src/events/listeners/__test__/OrderCreatedListener.test.ts
+++ tickets/src/events/listeners/__test__/OrderCreatedListener.test.ts
@@ -38,7 +38,7 @@ it('sets the user id of the ticket', async () => {
   const { listener, ticket, data, msg } = await setup();
   await listener.onMessage(data, msg);
 
-  const updatedTicket = Ticket.findById(ticket.id);
+  const updatedTicket = await Ticket.findById(ticket.id);
 //@ts-ignore
   expect(updatedTicket!.orderId).toEqual(data.id)
 })
@@ -47,4 +47,11 @@ it('acks the message', async () => {
   const { listener, ticket, data, msg } = await setup();
   await listener.onMessage(data, msg);
   expect(msg.ack).toHaveBeenCalled();
+})
+
+it('publishes a ticket updated event', async () => {
+  const { listener, ticket, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 })
