97b809c0b05add0a05de50d1e35def140ffc3197
Leftyxiv <manny@aledoux.net>
2021-05-06 10:09:22 -0400

    added expiration deployment

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,19 +1,10 @@
-import mongoose from "mongoose";
-
-import { app } from "./app";
 import { natsWrapper } from './natsWrapper';
-import { OrderCreatedListener } from './events/listeners/OrderCreatedListener';
-import { OrderCancelledListener } from './events/listeners/OrderCancelledListener';
+
 
 
 
 const start = async () => {
-  if (!process.env.jwt_key) {
-    throw new Error("jwt key not defined");
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error("mongo_uri must be defined");
-  }
+
   if (!process.env.NATS_URL) {
     throw new Error("nats_url must be defined");
   }
@@ -37,21 +28,11 @@ const start = async () => {
   // @ts-ignore
   process.on("SIGTERM", () => natsWrapper.client.close());
   
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log("Mongodb connected...");
   } catch (err) {
-    console.error(err);
+    
   }
-  app.listen(9000, () => {
-    console.log("listening on port 9000 **LOVE**");
-  });
+
+ 
 };
 
 start();
