fb0f71701d37865bbc283c8a18532d0f98f45c6a
Leftyxiv <manny@aledoux.net>
2021-05-07 08:57:09 -0400

    added both listeners to the index of payments

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,3 +1,5 @@
+import { OrderCancelledListener } from './events/listeners/orderCancelledListener';
+import { OrderCreatedListener } from './events/listeners/orderCreatedListener';
 import mongoose from "mongoose";
 
 import { app } from "./app";
@@ -35,7 +37,8 @@ const start = async () => {
   // @ts-ignore
   process.on("SIGTERM", () => natsWrapper.client.close());
   
-
+    new OrderCancelledListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
