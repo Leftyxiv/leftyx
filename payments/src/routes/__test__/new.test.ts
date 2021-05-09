06777a62b7ebd1df699b8f620d5f107f957a0f68
Leftyxiv <manny@aledoux.net>
2021-05-09 08:02:09 -0400

    fixed test suite for payments

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -7,15 +7,18 @@ import { stripe } from '../../stripe';
 import { Payment } from '../../models/Payment';
 
 
-it("returns an error when the order does not exist", async () => {
+it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin())
-    .send({ token: "567567", orderId: mongoose.Types.ObjectId().toHexString() })
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: mongoose.Types.ObjectId().toHexString(),
+    })
     .expect(404);
 });
 
-it("returns an error when trying to pay for someone elses order", async () => {
+it('returns a 401 when purchasing an order that doesnt belong to the user', async () => {
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId: mongoose.Types.ObjectId().toHexString(),
@@ -26,15 +29,17 @@ it("returns an error when trying to pay for someone elses order", async () => {
   await order.save();
 
   await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin())
-    .send({ token: "567567", orderId: order.id })
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: order.id,
+    })
     .expect(401);
 });
 
-it("returns an error when purchasing a cancelled order", async () => {
+it('returns a 400 when purchasing a cancelled order', async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
-
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId,
@@ -45,15 +50,18 @@ it("returns an error when purchasing a cancelled order", async () => {
   await order.save();
 
   await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin(userId))
-    .send({ orderId: order.id, token: "4534" })
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      orderId: order.id,
+      token: 'asdlkfj',
+    })
     .expect(400);
 });
 
-it("returns a 204 with valid inputs", async () => {
+it('returns a 201 with valid inputs', async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
-  const price = Math.floor((Math.random() * 100000));
+  const price = Math.floor(Math.random() * 100000);
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId,
@@ -62,19 +70,27 @@ it("returns a 204 with valid inputs", async () => {
     status: OrderStatus.Created,
   });
   await order.save();
-  console.log(order)
-  console.log(await Order.find())
+
   await request(app)
-    .post("/api/orders")
-    .set("Cookie", global.signin(userId))
-    .send({ token: "tok_visa", orderId: order.id })
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
     .expect(201);
 
-const stripeCharges = await stripe.charges.list({ limit: 15 });
-const stripeCharge = stripeCharges.data.find(charge => charge.amount === price * 100);
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+  const stripeCharge = stripeCharges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
 
-expect(stripeCharge).toBeDefined();
+  expect(stripeCharge).toBeDefined();
+  expect(stripeCharge!.currency).toEqual('usd');
 
-const payment = await Payment.findOne({ orderId: order.id, stripeId: stripeCharge!.id })
-expect(payment).not.toBeNull();
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+  expect(payment).not.toBeNull();
 });
